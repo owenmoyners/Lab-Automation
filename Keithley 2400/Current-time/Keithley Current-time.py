47d4719c5c9a8e23bc47a1fd833ca3c2383ab460
owenmoyners <63643276+owenmoyners@users.noreply.github.com>
2023-01-20 11:57:44 +0000

    Add files via upload

---
+++ Keithley 2400/Current-time/Keithley Current-time.py
@@ -0,0 +1,142 @@
+'''
+SCRIPT TO RUN AN CURRENT VS TIME FOR 2602A KEITHLEY - CHANNEL B (HAVE FOUND THEM NOT TO WORK FOR OTHER KINDS)
+
+CHANGE PARAMETERS IN PARAMETER SECTION AND RUN 
+
+MAKE SURE EVERYTHING IS CONNECTED TO THE PC 
+
+SAVES AS CSV FILE AT PATH SPECIFIED 
+
+ANY PROBLEMS ASK ME - OWEN MOYNIHAN
+'''
+
+
+'''Adding packages'''
+import keyboard
+import pyvisa
+import time
+from datetime import date
+from datetime import datetime
+import os 
+import csv
+import matplotlib.pyplot as plt
+
+'''INPUT PARAMETERS HERE'''
+
+#voltage 
+V = 0
+
+#time 
+T = 5 # minutes
+
+#current limit
+limit = 0.03
+
+#Path for file to be saved 
+file_name = 'A3650-timelight0V'
+
+#COPY AND PASTE PATH AS IS, NO NEED TO ADD SECOND SLASHES 
+path = '\\FS1\Docs2\owen.moynihan\My Documents\Project work\Testing Results\Surface passivation run\Current time'
+
+keithley_address = 'GPIB0::26::INSTR'
+
+
+'''Setting up test : Dont change anything from here'''
+
+
+r_path=  repr(path)[1:-1] #changing path to raw string to avoid back slash error
+I = []
+T_l = []
+
+T_s = T*60
+T_s = int(T_s)
+T_l = list(range(T_s))
+ 
+index = 0 #index with for loops
+
+#Setting up address and opening communication with Keithley 
+rm = pyvisa.ResourceManager()
+print(rm.list_resources())
+
+#Make sure it is the right address for Keithley 
+keithley = rm.open_resource(keithley_address)
+
+#Checking on console that it is correct address
+print(keithley.query("*IDN?"))
+
+keithley.write("smua.reset()")  
+keithley.write("errorqueue.clear()")
+keithley.write("smua.source.autorangei = smua.AUTORANGE_ON")
+keithley.write("smua.source.limiti = " + str(limit))
+
+#Going to display screen
+#keithley.write('display.screen = 0')
+
+'''Beginning test'''
+keithley.write("smua.source.func = smua.OUTPUT_DCVOLTS")
+
+#Changing to 0 just before turning it on 
+keithley.write("smua.source.levelv = 0")
+    
+#Turns on output 
+keithley.write("smua.source.output = smua.OUTPUT_ON ")
+
+keithley.write("smua.source.levelv = " + str(V))
+    
+for t in T_l:
+    keithley.write('print(smua.measure.i())') #careful with the quotation mark placement.
+    i = keithley.read() #reading current from keithley 
+    i = i[:-1] # taking off some string line
+    i = float(i)
+    I.append(i) #saving value to list
+    time.sleep(1)
+    
+    
+keithley.write("smua.source.output = smua.OUTPUT_OFF ")   
+
+
+'''Saving data to a csv file'''
+
+os.chdir(r_path)  #changing path 
+
+
+with open(file_name + ".csv", 'w', newline='') as csvfile:
+    
+    fieldnames =['Time (s)', 'Current (A)' ] #setting the row titles
+    
+    thewriter = csv.DictWriter(csvfile, fieldnames=fieldnames)
+    
+    thewriter.writeheader()
+
+    
+    for i in I:
+        
+        t = T_l[index]
+        thewriter.writerow({'Time (s)':t, 'Current (A)':i}) #adding lists to rows
+        index = index + 1
+        
+
+#PLOTTING Current time 
+plt.plot(T_l, I, linewidth = 0.4)
+plt.title(file_name)
+plt.gca().invert_yaxis()
+plt.xlabel("Time (s)")
+plt.ylabel("Current (A)")
+plt.savefig(f"{file_name}.png", dpi=500 , bbox_inches='tight')
+plt.show()
+
+
+plt.plot(T_l/60, I, linewidth = 0.4)
+plt.xlabel("Time (s)")
+plt.ylabel("Current (A)")
+plt.show()
+
+
+print("\nSee plot in plot window")
+print(f"Saved in {r_path}")
+
+    
+    
+    
+
+
---
+++ Keithley 2400/photoelectric chemical etching setup/Old scripts/photoelectic_chemical_etching_setup_v1.0.py
@@ -0,0 +1,143 @@
+# -*- coding: utf-8 -*-
+"""
+SCRIPT TO MEASURE CURRENT VS TIME IN PHOTOCHEMICAL ETCHING SETUP
+
+JUST INPUT VALUES IN BELOW SECTION AND RUN
+
+ANY PROBELMS ASK ME - OWEN MOYNIHAN
+owen.moynihan@tyndall.ie
+"""
+
+''' INPUT VALUES HERE '''
+
+V = 1 # in volts
+
+T = 5  #in minutes
+
+file_name = 'my file name' # do not specify file type (saves as csv)
+
+LED_power = "10mW" #record LED power
+
+Etchant = "FeCl3 0.1M" #record etchant and molarity
+
+#Enter path to save file
+path = '\\FS1\Docs2\owen.moynihan\My Documents\Project work'
+
+
+
+
+
+
+
+
+
+
+'''PROGRAM SETUP - DONT CHANGE ANYTHING BELOW '''
+#adding packages 
+import pyvisa
+import time
+from datetime import date
+from datetime import datetime
+import os 
+import csv
+import matplotlib.pyplot as plt
+from pylab import *
+
+#setting up connection
+rm = pyvisa.ResourceManager()
+rm.list_resources()
+Keithley = rm.open_resource('GPIB0::24::INSTR') #GPIB ADDRESS FOR KEITHLEY
+
+#assigning variables
+r_path=  repr(path)[1:-1] #changing path to raw string to avoid back slash error
+
+file_csv = file_name + ".csv"
+File = os.path.isfile(file_csv)
+#print(File)
+
+if File == True:
+    print("WARNING - READ BELOW:")
+    time.sleep(1)
+    print(f"You will overwrite pervious {file} file")
+    print("Right click this window and press 'Quit' to cancel, or left click and press anything to continue")
+    input("Press Enter to continue: ")
+    print("Continuing...")
+I = []
+T_l = []
+
+T_s = T*60 + 1
+T_s = int(T_s)
+T_l = list(range(T_s))
+
+#compliance = 20e-3 # in amps
+
+os.chdir(r_path)  #changing path 
+
+index = 0 #index with for loops
+
+#Applying voltage to keithley and measuring current 
+Keithley.write("*RST")
+Keithley.timeout = 25000
+
+Keithley.write(":SENS:FUNC:CONC OFF")
+Keithley.write(":SOUR:FUNC VOLT")
+Keithley.write(":SENS:FUNC 'CURR'")
+#Keithley.write("SENS:CURR:PROT 10E-3")
+Keithley.write(":OUTP ON")
+
+Keithley.write(":SOUR:VOLT:LEV {V}".format(V=V))
+
+Keithley.write(":OUTP ON")
+print(f"Time (s): \t Current (A):")
+for t in T_l:
+    Keithley.query(":READ?")
+    i = Keithley.query_ascii_values(":FETC?")
+    i = i[1] #reading current from keithley 
+    i = float(i) #turning value into float
+    I.append(i) #saving value to list
+    print(f"{t} \t \t \t {i}") #printing values
+    time.sleep(1) #waiting 1 second
+
+
+Keithley.write(":OUTP OFF")
+
+
+#Saving data to a csv file
+with open(file_name + ".csv", 'w', newline='') as csvfile:
+    
+    fieldnames =['Time (s)', 'Current (A)' ] #setting the row titles
+    
+    thewriter = csv.DictWriter(csvfile, fieldnames=fieldnames)
+    
+    thewriter.writerow({'Wavelength (nm)': f"Voltage = {V}V, LED power = {LED}, Etchant = {etchant}" })     
+    
+    thewriter.writeheader()
+
+    
+    for i in I:
+        
+        t = T_l[index]
+        thewriter.writerow({'Time (s)':t, 'Current (A)':i}) #adding lists to rows
+        index = index + 1
+
+rc('axes', linewidth=2)
+plt.rcParams["font.weight"] = "bold"
+plt.rcParams["axes.labelweight"] = "bold"   
+
+#plotting graph
+plt.figure()
+plt.plot(T_l, I,color="#0000a8ff", linewidth = 1, marker = 'D',
+         markerfacecolor = 'red', markersize = 4)
+plt.title(file_name)
+matplotlib.pyplot.figtext(0.15,0.8, "V = {V}V".format(V=V), fontsize = "large")
+plt.grid(True, alpha=0.5)
+plt.xlabel("Time (s)")
+plt.ylabel("Current (A)")
+plt.savefig(f"{file_name}.png", dpi=500 , bbox_inches='tight')
+
+plt.show()
+
+
+
+print("\nSee plot in plot window")
+print(f"Saved in {r_path}")
